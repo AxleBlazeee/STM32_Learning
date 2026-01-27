1525a96d680b96c66a1be26738a5b4f4964cd702
Abhishek <abhishekrawat069@gmail.com>
2026-01-27 12:13:23 +0100

    Added some extra comments

--- Blink_usingRegisterAddressing/Src/main.c
+++ Blink_usingRegisterAddressing/Src/main.c
@@ -4,14 +4,14 @@
 
 int main(void)
 {
-	RCC |= (1<<2);
-	GPIOC_MODER |= (1<<26);
-	GPIOC_MODER &= ~(1<<27);
+	RCC |= (1<<2);               //Enables clock to GPIOC
+	GPIOC_MODER |= (1<<26);      //To set pin13 in OUTPUT mode
+	GPIOC_MODER &= ~(1<<27);     //To set pin13 in OUTPUT mode
 	while(1)
 	{
-		GPIOC_ODR |= (1<<13);
-		for(int i=0; i<100000; i++){}
-		GPIOC_ODR &= ~(1<<13);
-		for(int i=0; i<100000; i++){}
+		GPIOC_ODR |= (1<<13);			//To make pin13 state HIGH
+		for(int i=0; i<100000; i++){}	//To give some delay
+		GPIOC_ODR &= ~(1<<13);			//To make pin13 state LOW
+		for(int i=0; i<100000; i++){}	//To give some delay
 	}
 }
