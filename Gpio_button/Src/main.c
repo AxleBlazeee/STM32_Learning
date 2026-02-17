04f68b02d0ea584080dd6770507aa5c200c08107
AxleBlazeee <abhishekrawat069@gmail.com>
2026-02-17 17:07:11 +0100

    Added comments for code understanding

--- Gpio_button/Src/main.c
+++ Gpio_button/Src/main.c
@@ -1,19 +1,19 @@
-#include "stm32f4xx.h"
+#include "stm32f4xx.h"  // Including all the register addresses
 
 int main(void)
 {
-	RCC->AHB1ENR |= (1<<0);
-	RCC->AHB1ENR |= (1<<2);
+	RCC->AHB1ENR |= (1<<0); //Clock enable for GPIOA
+	RCC->AHB1ENR |= (1<<2);	//Clock enable for GPIOC
 
-	GPIOC->MODER |= (1<<26);
-	GPIOA->PUPDR |= (1<<0);
+	GPIOC->MODER |= (1<<26); //Output mode on pin PC13
+	GPIOA->PUPDR |= (1<<0); //Using internal Pull up resistor on pin PA0
 
 	while(1)
 	{
-		if((GPIOA->IDR & 0x00000001)== 0x00000001)
-			GPIOC->ODR |= (1<<13);
-		else
-			GPIOC->ODR &=~ (1<<13);
+		if((GPIOA->IDR & 0x00000001)== 0x00000001) //If button pressed 
+			GPIOC->ODR |= (1<<13);		   //led ON
+		else					   //Else
+			GPIOC->ODR &=~ (1<<13);		   //led OFF
 //		if((GPIOA->IDR & 0x00000001)==0x00000000)
 //			GPIOC->ODR &=~ (1<<13);
 //		else
